3cff5a2493cf24cebb8cdda8139f17329d1022b2
Marc <marchoyle27@gmail.com>
2018-05-10 11:53:26 -0700

    Updated spacing

--- env1.scm
+++ env1.scm
@@ -1,21 +1,21 @@
 ;; File: env1.scm
 
 (define make-empty-env
-	(lambda ()
-		(list)))
+    (lambda ()
+        (list)))
 
 (define apply-env
-	(lambda (env v)
-		(cond
-			((null? env) 
-				(error "error: variable not found in environment"))
-			(else
-				(cond 
-					((equal? v (car env))
-						(car (cdr env)))
-					(else
-						(apply-env (car (cdr (cdr env))) v)))))))
+    (lambda (env v)
+        (cond
+            ((null? env) 
+                (error "error: variable not found in environment"))
+            (else
+                (cond 
+                    ((equal? v (car env))
+                        (car (cdr env)))
+                    (else
+                        (apply-env (car (cdr (cdr env))) v)))))))
 
 (define extend-env
-	(lambda (v val env)
-		(list v val env)))
+    (lambda (v val env)
+        (list v val env)))
